efaedc9c1c7606373ac4f026bee869ff2fbceda8
Patrick Barton Grace <132808103+pbgcommits@users.noreply.github.com>
2024-12-13 10:30:27 +0000

    Level 1 (almost)

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -19,13 +19,25 @@ Item = namedtuple('Item', 'type, description, amount, quantity')
 def validorder(order: Order):
     net = 0
 
+    # Test 8 - shouldn't be able to make an order worth more than 99999
+    MAX_AMOUNT_PAYABLE = 99999
+    amount_payable = 0
+
     for item in order.items:
         if item.type == 'payment':
+            if item.amount + net >= 1e16:
+                ... # idk how to deal with this (underflow?)
             net += item.amount
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            product_cost = item.amount * item.quantity
+            net -= product_cost
+            amount_payable += product_cost
         else:
             return "Invalid item type: %s" % item.type
+        if amount_payable > MAX_AMOUNT_PAYABLE:
+            return "Total amount payable for an order exceeded"
+        # Test 7 - floating point error
+        net = round(net, ndigits=2)
 
     if net != 0:
         return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
